9a972e631c62463cbdcec325f672699deaca29f5
Ahmed <ahmed@gmail.com>
2023-02-07 19:21:18 +0200

    until creat post feature

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+
 let sidbar = document.querySelectorAll(".part-2 >div")
 
 sidbar.forEach((element)=>{
@@ -13,10 +14,11 @@ sidbar.forEach((element)=>{
 
 })
 
-// ******************************
-// Requests => show some posts
-
-// ******************************
+// *****************************************************
+// read Posts
+readPost()
+function readPost() {
+    // Requests => show some posts
 fetch("https://tarmeezacademy.com/api/v1/tags/1/posts")
 .then((responseJson)=>{
   return  responseJson.json()
@@ -82,24 +84,158 @@ fetch("https://tarmeezacademy.com/api/v1/tags/1/posts")
     console.log("error")
 })
 
-// ***************************
-let login = document.getElementById("login")
+}
 
-login.addEventListener("click",()=>{[
+// *****************************************************
 
-    location.href= "../login and rigester/login.html"
+// go to Login => whene click login
+function goToLogin () {
+    let login = document.getElementById("login")
 
-]})
+    login.addEventListener("click",()=>{
+    
+        location.href= "../login and rigester/login.html"
+    })
+}
+goToLogin ()
 
+// *****************************************************
 
+// appear and disappear => login &Logout & Buttom => start
+function loginLogoutButtom(){
+    if(localStorage.getItem("token") !==null){
+        document.getElementById("login").style.display = "none"
+        document.getElementById("namePerson").textContent =JSON.parse(localStorage.getItem("userData")).name
+        document.getElementById("whatIsOnYourMind").placeholder= `what is on your mind, ${JSON.parse(localStorage.getItem("userData")).name}`
+        var emptyObject = JSON.parse(localStorage.getItem("userData")).profile_image;
+        // update defult picture (user) => it depends user has picture or not
+        if (isObjEmptyPictureProfile(emptyObject)) {
+            console.log("empty => stil defult")
+        }else{
+            console.log("there is picture => it shoude be change to it")
+        }
+    }else{
+        document.getElementById("login").style.display = "block" 
+        document.getElementById("logout").style.display = "none"
+        
+        // todo => i wanna apear measage whene login
+    }
+}
+loginLogoutButtom()
 
+// *****************************************************
 
+// appear and disappear => login &Logout & Buttom => end
+function logout(){
+    localStorage.removeItem("token")
+    localStorage.removeItem("userData")
+    loginLogoutButtom()
+    message("your logout has been confirmed","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_wbBfnys5rxvgITJDUK-vKqPbaCW2rKSp-w&usqp=CAU")
+  
+}
 
+// *****************************************************
+// message => as alert
+function message (mess,imge){
+    document.getElementById("countMessage").innerHTML =`
+        <div id="message">
+            <img src="${imge}">
+            <p id="contentMessage"> ${mess} </p>
+        </div> 
+    `
+    setTimeout( ()=>{
+        document.getElementById("countMessage").innerHTML = ""
+    },3000)
+}
 
+// *****************************************************
+// is Obj Empty Picture Profile
+function isObjEmptyPictureProfile (obj) {
+    return Object.keys(obj).length === 0;
+}
 
+// *****************************************************
+// creat post => open model
+function creatPost (){
+        // console.log(document.getElementsByClassName("new-post"))
+    if(localStorage.getItem("token") !==null){
+        // in console
+        console.log("you can")
+        // open model
+        document.getElementById("model").style.visibility = "visible"
+        // confused content
+        document.getElementById("home").style.filter = "blur(3px)"
 
+        
+    }else{
+        // in console
+        console.log("you can not")
+        // go to login first
+        location.href= "../login and rigester/login.html"
 
+    }
+}
 
+// *****************************************************
+// close models &  clear content
+document.getElementById("close-model-1").addEventListener("click",()=>{
+    document.getElementById("model").style.visibility = "hidden"
+      document.getElementById("home").style.filter = "none"
+      
+    })
+    document.getElementById("close-model-2").addEventListener("click",()=>{
+        document.getElementById("model").style.visibility = "hidden"
+        document.getElementById("home").style.filter = "none"
+})
 
+// *****************************************************
+function craetPost() {
+      // ---------creat post => start by axios--------- 
+        // baseUrl
+        let baseUrl = "https://tarmeezacademy.com/api/v1/" 
+        let craetPostt = document.getElementById("craetPostt")
+        craetPostt.addEventListener("click",function(){
+            let title = document.getElementById("title").value
+            let content  = document.getElementById("content").value    
+            let fileImage = document.getElementById("fileImage").files[0]    
+            let token =localStorage.getItem("token")
+            // formData =>body params
+            let formData = new FormData()
+            formData.append("title",title)
+            formData.append("body",content)
+            formData.append("image",fileImage)
+        // let body = {
+        //     "title":title,
+        //     "body":content,
+        // }
+            // full url to login
+            let url =`${baseUrl}posts`
+            axios.post(url,formData,{
+                headers:{
+                "authorization": `Bearer ${token}`,
+                }
+            })
+        .then(response=>{
+            // console.log(response.data)
+            console.log(response)
+            // hidde model
+            document.getElementById("model").style.visibility = "hidden"
+            document.getElementById("home").style.filter = "none"
+            // message => alert
+            message ("your post was created","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_wbBfnys5rxvgITJDUK-vKqPbaCW2rKSp-w&usqp=CAU")
+        //    read posts
+            readPost()
+          
+            })
+            .catch(data=>{
+                console.log(data)
+                // console.log(`eroor message  ${data.response.data.message}  `)
+                message (data.message,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2odXc3DVvMonj_yuXb8q5kR0N-1ndG8Fv9VSufsUAf60UjwbOPkWZVbPmZVuOf3-PoB0&usqp=CAU")
+                // message (data.message,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_wbBfnys5rxvgITJDUK-vKqPbaCW2rKSp-w&usqp=CAU")
+        })
+    }
+    )
+    // ---------creat post => end by axios--------- 
 
-
+}
+craetPost()
--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+// const { default: axios } = require("axios")
+
 // ---------show and hidden pass => start-----------
 let e = document.querySelector(".shHi")
 e.addEventListener("click", () =>{
@@ -10,43 +12,87 @@ e.addEventListener("click", () =>{
     }
 })
 // ---------show and hidden pass => end-----------
-// ---------send data from login => start--------- 
+// ---------send data from login => start by ajax--------- 
 
-let login = document.getElementById("login")
-login.addEventListener("click",function(){
-    let UserName = document.getElementById("UserName").value
-    let password = document.getElementById("password").value    
-    let req =new XMLHttpRequest()
-    req.open("POST"," https://tarmeezacademy.com/api/v1/login")
-    req.setRequestHeader("Accept", "application/json");
-    req.setRequestHeader("Content-type","application/json");
-    let body = JSON.stringify({
-        "username":UserName,
-        "password":password,
-    })
-    // "username":"ahmeddd123",
-    // "password": "123456789",
-    // "name":"ali"
-    req.send(body)
-    req.onload = function(){
-        if(req.status >=200 && req.status <300 ){
-        let responseSucsees = JSON.parse(req.response)
-        // window.localStorage.setItem("token",JSON.stringify(req.response))
-        // location.href= "../login and rigester/home.html"
-    }else{
-        let responseUnsucsees = JSON.parse(req.response)
-        alert(responseUnsucsees.error)
-        console.log(`response is unSucsees:. status ${req.status}  readyState  ${req.readyState} `)
-    }
-}
-}
-)
-
-
-
+// let login = document.getElementById("login")
+// login.addEventListener("click",function(){
+//     let UserName = document.getElementById("UserName").value
+//     let password = document.getElementById("password").value    
+//     let req =new XMLHttpRequest()
+//     req.open("POST","https://tarmeezacademy.com/api/v1/login")
+//     req.setRequestHeader("Accept", "application/json");
+//     req.setRequestHeader("Content-type","application/json");
+//     let body = JSON.stringify({
+//             username:"ahmeddd123",
+//             password :123456789,
+//     })
+//     // "username":"ahmeddd123",
+//     // "password": "123456789",
+//     // "name":"ali"
+//     req.send(body)
+//     console.log(`sent `)
+//     req.onload = function(){
+//             if(req.status >=200 && req.status <300 ){
+//                 let responseSucsees = JSON.parse(req.response)
+//         // window.localStorage.setItem("token",JSON.stringify(req.response))
+//         // location.href= "../login and rigester/home.html"
+//     }else{
+//             let responseUnsucsees = JSON.parse(req.response)
+//             alert(responseUnsucsees.error)
+//         console.log(`response is unSucsees:. status ${req.status}  readyState  ${req.readyState} `)
+//     }
+// }
+// }
+// )
+// ---------send data from login => end by ajax--------- 
 
 
 
 
 // window.localStorage.clear()
+
+    function message (mess){
+        document.getElementById("countMessage").innerHTML =`
+            <div id="message">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2odXc3DVvMonj_yuXb8q5kR0N-1ndG8Fv9VSufsUAf60UjwbOPkWZVbPmZVuOf3-PoB0&usqp=CAU">
+                <p id="contentMessage">${mess}</p>
+            </div> 
+        `
+        setTimeout( ()=>{
+            document.getElementById("countMessage").innerHTML = ""
+        },3000)
+    }
+
+     // baseUrl
+     let baseUrl = "https://tarmeezacademy.com/api/v1/" 
+     let signup = document.getElementById("signup")
+     signup.addEventListener("click",function(){
+         console.log("you clicked on signup")
+         let UserName = document.getElementById("username-signup").value
+         let password = document.getElementById("password-signup").value    
+         let name = document.getElementById("name-signup").value   
+         console.log(UserName,name,password)
+         // body params
+         let body = {
+             "username":UserName,
+             "password":password,
+             "name":name
+         }
+         // full url to login
+         let url =`${baseUrl}register`
+         axios.post(url,body)
+    .then(response=>{
+        // console.log(response.data)
+        localStorage.setItem("token",response.data.token)
+        localStorage.setItem("userData",JSON.stringify(response.data.user))
+        location.href= "../login and rigester/home.html"
+        })
+    .catch(error=>{
+        console.log(error)
+        console.log(`eroor message  ${error.response.data.message}  `)
+        message(error.response.data.message)
+    //  alert(error.response.data.message)
+     })
+ }
+ )
 
